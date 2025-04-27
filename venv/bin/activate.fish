56f9cafa8e08a7459b7dfcece2b8a279cd1be60a
Emerson-91 <emerson.hcdossantos@gmail.com>
2025-04-26 20:25:41 -0400

    alteracoes no venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/emerson/Projetos/TI-Panel/venv
+set -gx VIRTUAL_ENV /home/emerson/Projetos/Painel-TI/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
